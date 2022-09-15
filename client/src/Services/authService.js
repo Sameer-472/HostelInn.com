d8c111c6474c04a585c7a1f47d5ba046c503ae4d
Anas Ali <asheryar12@gmail.com>
2022-09-15 08:21:30 +0500

    auth

---
+++ client/src/Services/authService.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const URL = "http://localhost:8000";
+
+export const register=async(endpoint,payload)=>{
+    try {
+        const {data}=await axios.post(`${URL}/signUp/${endpoint}`,payload)
+        return data;
+    } catch (error) {
+        console.log(error.message);
+    }
+
+}
+
+export const login=async(endpoint,payload)=>{
+    try {
+        const {data}=await axios.post(`${URL}/signIn/${endpoint}`,payload)
+        if(data.token){
+            localStorage.setItem("user",JSON.stringify(data))
+        }
+        return data;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
