ac681516bc40da2d922db95891728e15950f0645
Faraz <farazatgithub@gmail.com>
2022-11-13 21:48:35 -0800

    Test2

--- client/src/Redux/Reducers/auth.js
+++ client/src/Redux/Reducers/auth.js
@@ -4,8 +4,6 @@ import {
     REGISTER_SUCCESS , REGISTER_FAIL , LOGIN_SUCCESS , LOGIN_FAIL , LOGOUT
 } from '../Actions/actionType'
 
-I am faraz ahmed
-
 const user = JSON.parse(localStorage.getItem("user"));
 
 const initialState = user ? {isLoggedIn: true , user}: {isLoggedIn: false , user: null};
