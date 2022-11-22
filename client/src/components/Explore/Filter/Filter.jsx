113419f0c0b1e312151b42c419c6448f038c942a
Faraz <farazatgithub@gmail.com>
2022-11-22 12:27:42 +0500

    Styles

--- client/src/components/Explore/Filter/Filter.jsx
+++ client/src/components/Explore/Filter/Filter.jsx
@@ -1,78 +1,15 @@
 import React from "react";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
+import { MenuItem } from "@mui/material";
 
 import {
-  Box,
-  styled,
-  TextField,
-  Button,
-  MenuItem
-} from "@mui/material";
-
-const FilterContainer = styled(Box)`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 1em;
-`;
-
-const FilterLeftBox = styled(Box)`
-  display: flex;
-  align-items: center;
-  width: 45%;
-`;
-
-const FilterRightBox = styled(Box)`
-  width: 15%;
-  margin-right: 1em;
-`;
-
-const FilterButton = styled(Button)`
-  font-family: inherit;
-  background-color: #fafafa;
-  color: #606060;
-  border: 2px solid lightgrey;
-  border-radius: 10px;
-  margin-right: 1em;
-  font-weight: bold;
-  text-transform: uppercase;
-  margin-left: 1em;
-  width: 35%;
-  &:hover {
-    background-color: #e5dbdb;
-  }
-`;
-const SortButton = styled(TextField)`
-  font-family: inherit;
-  background-color: #fafafa;
-  color: #606060;
-  border: 2px solid lightgrey;
-  width: 35%;
-  margin-right: 1em;
-  font-weight: bold;
-  border-radius: 10px;
-  display: flex;
-  align-items: center;
-  padding-top: 0.5em;
-  text-align: center;
-  text-transform: uppercase;
-  &:hover {
-    background-color: #e5dbdb;
-  }
-`;
-
-const MapButton = styled(Button)`
-  font-family: inherit;
-  border: 2px solid lightgrey;
-  background-color: #fafafa;
-  color: #606060;
-  font-weight: bold;
-  border-radius: 10px;
-  text-transform: uppercase;
-  &:hover {
-    background-color: #e5dbdb;
-  }
-`;
+  FilterContainer,
+  FilterLeftBox,
+  FilterButton,
+  SortButton,
+  FilterRightBox,
+  MapButton,
+} from "./FilterStyles";
 
 function Filter() {
   return (
--- client/src/components/Explore/Quality/Quality.jsx
+++ client/src/components/Explore/Quality/Quality.jsx
@@ -1,83 +1,55 @@
 import React from "react";
 
-import {
-  Box,
-  styled,
-  Typography
-} from "@mui/material";
-
-const QualitiesBoxContainer = styled(Box)`
-  display: flex;
-  align-items: center;
-  margin: 0 1.5em;
-`;
-
-const Item = styled(Box)`
-  margin-left: 1em;
-`;
-
-const TypeGraphyBox = styled(Typography)`
-  font-family: "Lato", sans-serif;
-  padding: 0 0.5em;
-  margin-left: 1em;
-  text-transform: uppercase;
-  font-weight: bold;
-  color: #606060;
-  &:hover {
-    border-bottom: 2px solid #ff6600;
-    cursor: pointer;
-  }
-`;
-
+import { QualitiesBoxContainer, Item, TypeGraphyBox } from "./QualityStyles";
 
 function Quality() {
   return (
-      <QualitiesBoxContainer>
-        <Item>
-          {" "}
-          <TypeGraphyBox variant="body2">Price</TypeGraphyBox>{" "}
-        </Item>
-        <Item
-          sx={{
-            borderLeft: "1px solid #B2B2B2",
-          }}
-        >
-          {" "}
-          <TypeGraphyBox variant="body2">Room</TypeGraphyBox>{" "}
-        </Item>
-        <Item
-          sx={{
-            borderLeft: "1px solid #B2B2B2",
-          }}
-        >
-          {" "}
-          <TypeGraphyBox variant="body2">Rating</TypeGraphyBox>{" "}
-        </Item>
-        <Item
-          sx={{
-            borderLeft: "1px solid #B2B2B2",
-          }}
-        >
-          {" "}
-          <TypeGraphyBox variant="body2">Facilities</TypeGraphyBox>{" "}
-        </Item>
-        <Item
-          sx={{
-            borderLeft: "1px solid #B2B2B2",
-          }}
-        >
-          {" "}
-          <TypeGraphyBox variant="body2">University</TypeGraphyBox>{" "}
-        </Item>
-        <Item
-          sx={{
-            borderLeft: "1px solid #B2B2B2",
-          }}
-        >
-          {" "}
-          <TypeGraphyBox variant="body2">Property Type</TypeGraphyBox>{" "}
-        </Item>
-      </QualitiesBoxContainer>
+    <QualitiesBoxContainer>
+      <Item>
+        {" "}
+        <TypeGraphyBox variant="body2">Price</TypeGraphyBox>{" "}
+      </Item>
+      <Item
+        sx={{
+          borderLeft: "1px solid #B2B2B2",
+        }}
+      >
+        {" "}
+        <TypeGraphyBox variant="body2">Room</TypeGraphyBox>{" "}
+      </Item>
+      <Item
+        sx={{
+          borderLeft: "1px solid #B2B2B2",
+        }}
+      >
+        {" "}
+        <TypeGraphyBox variant="body2">Rating</TypeGraphyBox>{" "}
+      </Item>
+      <Item
+        sx={{
+          borderLeft: "1px solid #B2B2B2",
+        }}
+      >
+        {" "}
+        <TypeGraphyBox variant="body2">Facilities</TypeGraphyBox>{" "}
+      </Item>
+      <Item
+        sx={{
+          borderLeft: "1px solid #B2B2B2",
+        }}
+      >
+        {" "}
+        <TypeGraphyBox variant="body2">University</TypeGraphyBox>{" "}
+      </Item>
+      <Item
+        sx={{
+          borderLeft: "1px solid #B2B2B2",
+        }}
+      >
+        {" "}
+        <TypeGraphyBox variant="body2">Property Type</TypeGraphyBox>{" "}
+      </Item>
+    </QualitiesBoxContainer>
   );
 }
 
