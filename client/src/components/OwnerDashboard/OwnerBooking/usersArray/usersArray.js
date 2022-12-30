0d60c46260a4e8344ad96e07f27fa3cd52439636
Faraz <farazatgithub@gmail.com>
2022-12-30 16:10:46 +0500

    ownerbooking

---
+++ client/src/components/OwnerDashboard/OwnerBooking/usersArray/usersArray.js
@@ -0,0 +1,116 @@
+import React from "react"
+
+import {
+    ButtonContainer,
+    AcceptButton,
+    RejectButton,
+  } from "../style";
+
+const rows = [
+    {
+      Name: "Annie Hall",
+      Room: "Standard single",
+      Date_Time: "Wednesday, 12-Dec-2022, 02:06:00 AM",
+      Hostel: "Hayat Girls Hostel",
+      Status: "Pending",
+      Button: (
+        <ButtonContainer>
+          <AcceptButton variant="standard">Accept</AcceptButton>
+          <RejectButton variant="standard">Reject</RejectButton>
+        </ButtonContainer>
+      ),
+    },
+    {
+      Name: "Marry Auther",
+      Room: "Basic Double Room",
+      Date_Time: " Wednesday, 12-Dec-2022, 02:06:00 AM",
+      Hostel: "Hayat Girls Hostel",
+      Status: "Pending",
+      Button: (
+        <ButtonContainer>
+          <AcceptButton variant="standard">Accept</AcceptButton>
+          <RejectButton variant="standard">Reject</RejectButton>
+        </ButtonContainer>
+      ),
+    },
+    {
+      Name: "Hina Perwez",
+      Room: "Standard single",
+      Date_Time: "Wednesday, 12-Dec-2022, 02:06:00 AM",
+      Hostel: "Hayat Girls Hostel",
+      Status: "Pending",
+      Button: (
+        <ButtonContainer>
+          <AcceptButton variant="standard">Accept</AcceptButton>
+          <RejectButton variant="standard">Reject</RejectButton>
+        </ButtonContainer>
+      ),
+    },
+    {
+      Name: "Hamza Raheel",
+      Room: "Standard single",
+      Date_Time: "Wednesday, 12-Dec-2022, 02:06:00 AM",
+      Hostel: "Hayat Boys Hostel",
+      Status: " Pending",
+      Button: (
+        <ButtonContainer>
+          <AcceptButton variant="standard">Accept</AcceptButton>
+          <RejectButton variant="standard">Reject</RejectButton>
+        </ButtonContainer>
+      ),
+    },
+    {
+      Name: "Raheem imran",
+      Room: " Basic 4 Bed Dorm Room",
+      Date_Time: "Wednesday, 12-Dec-2022, 02:06:00 AM",
+      Hostel: "Hayat Boys Hostel ",
+      Status: "Pending",
+      Button: (
+        <ButtonContainer>
+          <AcceptButton variant="standard">Accept</AcceptButton>
+          <RejectButton variant="standard">Reject</RejectButton>
+        </ButtonContainer>
+      ),
+    },
+    {
+      Name: "Sadaf altaf ",
+      Room: " Standard single",
+      Date_Time: "Wednesday, 12-Dec-2022, 02:06:00 AM",
+      Hostel: "Hayat Girls Hostel",
+      Status: "Pending",
+      Button: (
+        <ButtonContainer>
+          <AcceptButton variant="standard">Accept</AcceptButton>
+          <RejectButton variant="standard">Reject</RejectButton>
+        </ButtonContainer>
+      ),
+    },
+    {
+      Name:  "Kamran hyder",
+      Room: " Basic Double Room",
+      Date_Time: "Wednesday, 12-Dec-2022, 02:06:00 AM",
+      Hostel: "Hayat Boys Hostel",
+      Status: "Pending",
+      Button: (
+        <ButtonContainer>
+          <AcceptButton variant="standard">Accept</AcceptButton>
+          <RejectButton variant="standard">Reject</RejectButton>
+        </ButtonContainer>
+      ),
+    },
+    {
+      Name: "Sana Saad",
+      Room: "Basic 4 Bed Dorm Room",
+      Date_Time: "Wednesday, 12-Dec-2022, 02:06:00 AM",
+      Hostel: "Hayat Girls Hostel",
+      Status: "Pending",
+      Button: (
+        <ButtonContainer>
+          <AcceptButton variant="standard">Accept</AcceptButton>
+          <RejectButton variant="standard">Reject</RejectButton>
+        </ButtonContainer>
+      ),
+    }
+]
+
+export default rows
